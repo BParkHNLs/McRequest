58e03646b33973d788de7a3e4d8f9f662e249ee4
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2021-06-14 17:04:57 +0200

    added filter efficiencies

---
+++ efficiencies/V32_stats_Lxy1300_tkPt500MeV_lepPt400MeV_Bc_points.py
@@ -0,0 +1,47 @@
+from python.common  import Point,Config,getCtauEff
+
+# List of points to be generated
+#   - N.B.: mass must be a float
+
+
+m_ctau_eff_time_s = [
+
+(2.0,  10000.00, 1.87e-02, 18),
+(2.0,   1000.00, 1.02e-01, 20),
+(2.0,    100.00, 1.86e-01, 15),
+(2.0,     10.00, 1.83e-01, 15),
+(3.0,   1000.00, 8.48e-02, 15),
+(3.0,    100.00, 1.57e-01, 16),
+(3.0,     10.00, 1.59e-01, 22),
+(3.0,      1.00, 1.59e-01, 22),
+(4.5,    100.00, 3.60e-02, 19),
+(4.5,     10.00, 3.64e-02, 20),
+(4.5,      1.00, 3.64e-02, 19),
+(4.5,      0.10, 3.64e-02, 19),
+(5.5,     10.00, 1.31e-03, 48),
+(5.5,      1.00, 1.31e-03, 51),
+(5.5,      0.10, 1.38e-03, 50),
+(5.5,      0.00, 1.35e-03, 43),
+
+
+]
+
+points = []
+for m,ctau,eff,time in m_ctau_eff_time_s:
+  p   = Point(mass=m,ctau=ctau,vv=None,ismaj=True)
+  cfg = Config(nevtseff=5000,muoneff=eff,displeff=1.0,timeevt=time,timejob=12,contingency=3)
+  p.setConfig(cfg)
+  points.append(p)
+
+  # 
+
+  #print('mass={:.1f} vv={:.1e} before={:.1e}, after={:.1e}'.format(p.mass,p.vv,displEff[m][vv],getCtauEff(p.ctau*4.,1000.))) # 4 is the assumed beta*gamma factor...
+  #cfg.stamp()
+  
+
+
+
+
+
+
+
---
+++ efficiencies/V32_stats_Lxy1300_tkPt500MeV_lepPt400MeV_points.py
@@ -0,0 +1,50 @@
+from python.common  import Point,Config,getCtauEff
+
+# List of points to be generated
+#   - N.B.: mass must be a float
+
+
+m_ctau_eff_time_s = [
+
+(1.0,  10000.00, 1.69e-04, 427),
+(1.0,   1000.00, 1.34e-03, 70),
+(1.0,    100.00, 4.24e-03, 37),
+(1.0,     10.00, 4.86e-03, 29),
+(1.5,  10000.00, 1.02e-04, 678),
+(1.5,   1000.00, 8.42e-04, 98),
+(1.5,    100.00, 2.71e-03, 46),
+(1.5,     10.00, 2.99e-03, 49),
+(2.0,  10000.00, 4.68e-05, 1565),
+(2.0,   1000.00, 3.67e-04, 196),
+(2.0,    100.00, 1.25e-03, 75),
+(2.0,     10.00, 1.42e-03, 65),
+(3.0,   1000.00, 2.04e-03, 52),
+(3.0,    100.00, 4.82e-03, 33),
+(3.0,     10.00, 5.02e-03, 30),
+(3.0,      1.00, 5.02e-03, 30),
+(4.5,    100.00, 3.66e-04, 207),
+(4.5,     10.00, 4.39e-04, 165),
+(4.5,      1.00, 4.42e-04, 143),
+(4.5,      0.10, 4.42e-04, 131),
+
+]
+
+points = []
+for m,ctau,eff,time in m_ctau_eff_time_s:
+  p   = Point(mass=m,ctau=ctau,vv=None,ismaj=True)
+  cfg = Config(nevtseff=5000,muoneff=eff,displeff=1.0,timeevt=time,timejob=12,contingency=3.)
+  p.setConfig(cfg)
+  points.append(p)
+
+  # 
+
+  #print('mass={:.1f} vv={:.1e} before={:.1e}, after={:.1e}'.format(p.mass,p.vv,displEff[m][vv],getCtauEff(p.ctau*4.,1000.))) # 4 is the assumed beta*gamma factor...
+  #cfg.stamp()
+  
+
+
+
+
+
+
+
