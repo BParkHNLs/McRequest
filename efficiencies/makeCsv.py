51f544b0e3fa874605d5de180e288bbdaa83486e
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2021-06-29 10:50:51 +0200

    syntax change in csv file

--- efficiencies/makeCsv.py
+++ efficiencies/makeCsv.py
@@ -3,18 +3,19 @@ from V32_stats_Lxy1300_tkPt500MeV_lepPt400MeV_Bc_points import m_ctau_eff_time_s
 
 
 fout = open('request.csv', 'w')
-fout.write('dataset,fragment,events,generator,timeperevent,filterefficiency\n')
+fout.write('dataset,fragment,events,generator,time per event,filter efficiency\n')
 
 all_points = [(m,ctau,eff,time,'B') for m,ctau,eff,time in points_B] + [(m,ctau,eff,time,'Bc') for m,ctau,eff,time in points_Bc]
 
 for m,ctau,eff,time,spc in all_points:
   if ctau!=0.01:
-    ds = '{}ToNMuX_NToEMuPi_SoftQCD_b_mN{:.1f}_ctau{:.1f}mm_TuneCP5_13TeV_pythia8-evtgen'.format(spc,m,ctau)
+    name = '{}ToNMuX_NToEMuPi_SoftQCD_b_mN{:.1f}_ctau{:.1f}mm'.format(spc,m,ctau)
   else:
-    ds = '{}ToNMuX_NToEMuPi_SoftQCD_b_mN{:.1f}_ctau{:.2f}mm_TuneCP5_13TeV_pythia8-evtgen'.format(spc,m,ctau)
+    name = '{}ToNMuX_NToEMuPi_SoftQCD_b_mN{:.1f}_ctau{:.2f}mm'.format(spc,m,ctau)
 
+  ds = name + '_TuneCP5_13TeV-pythia8'
   path = 'genFragments/Generator/Pythia/BHNL/'
-  fragment = path + ds + '_cfi.py' 
+  fragment = path + name + '_TuneCP5_13TeV_pythia8-evtgen' + '_cfi.py' 
   events = 100000
   generator = 'pythia8'
   timeperevent = '{:.2e}'.format(time*eff)
