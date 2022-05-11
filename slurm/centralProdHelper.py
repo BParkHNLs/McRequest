4be663755bb748fe0008f2ee6f6a9a577d8a111b
anlyon <alyon@phys.ethz.ch>
2022-05-11 23:46:56 +0200

    modify the name of the Bc fragments to match that of the central lhe files

--- slurm/centralProdHelper.py
+++ slurm/centralProdHelper.py
@@ -510,9 +510,9 @@ ProductionFilterSequence = cms.Sequence(generator+BFilter+DoubleLeptonFilter+Tri
   def writeFragmentsBc(self):
     for p in self.points:
       if p.ctau!=0.01:
-       fragname = 'BcToHNLMuX_HNLToMuPi_SoftQCD_b_mHNL{:.1f}_ctau{:.1f}mm_TuneCP5_13TeV_pythia8-evtgen_cfi.py'.format(p.mass, p.ctau)
+       fragname = 'BcToNMuX_NToEMuPi_SoftQCD_b_mHNL{:.1f}_ctau{:.1f}mm_TuneCP5_13TeV_pythia8-evtgen_cfi.py'.format(p.mass, p.ctau)
       else:
-       fragname = 'BcToHNLMuX_HNLToMuPi_SoftQCD_b_mHNL{:.1f}_ctau{:.2f}mm_TuneCP5_13TeV_pythia8-evtgen_cfi.py'.format(p.mass, p.ctau)
+       fragname = 'BcToNMuX_NToEMuPi_SoftQCD_b_mHNL{:.1f}_ctau{:.2f}mm_TuneCP5_13TeV_pythia8-evtgen_cfi.py'.format(p.mass, p.ctau)
       p.fragname = fragname
       with open('{}/{}'.format(self.prodLabel,p.fragname), 'w') as f:
         tobewritten = '''
