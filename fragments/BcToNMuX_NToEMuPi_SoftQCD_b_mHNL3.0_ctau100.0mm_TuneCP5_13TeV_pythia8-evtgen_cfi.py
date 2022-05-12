528d57a808bb0217c957cf4bb3ceef77b578932c
anlyon <alyon@phys.ethz.ch>
2022-05-12 09:23:05 +0200

    fragments for the Bc grid added

---
+++ fragments/BcToNMuX_NToEMuPi_SoftQCD_b_mHNL3.0_ctau100.0mm_TuneCP5_13TeV_pythia8-evtgen_cfi.py
@@ -0,0 +1,117 @@
+
+import FWCore.ParameterSet.Config as cms
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+# Production Info
+configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('Bc -> mu N X, with long-lived N, m=3.0GeV, ctau=100.0mm'),
+    name = cms.untracked.string('Bc -> mu N X, with long-lived N, m=3.0GeV, ctau=100.0mm'),
+    version = cms.untracked.string('$1.0$')
+)
+
+BFilter = cms.EDFilter("MCMultiParticleFilter",
+   NumRequired = cms.int32(1),
+   AcceptMore = cms.bool(True),
+   ParticleID = cms.vint32(541),
+   PtMin = cms.vdouble(0.),
+   EtaMax = cms.vdouble(10.),
+   Status = cms.vint32(0), 
+)
+
+DoubleLeptonFilter = cms.EDFilter("MCParticlePairFilter",
+    MaxEta = cms.untracked.vdouble(1.55, 2.45),
+    MinEta = cms.untracked.vdouble(-1.55, -2.45),
+    MinPt = cms.untracked.vdouble(6.8, 1.0),
+    ParticleID1 = cms.untracked.vint32(-13, 13),
+    ParticleID2 = cms.untracked.vint32(-11, 11,-13,13),
+    MaxInvMass = cms.untracked.double(10.),
+    Status = cms.untracked.vint32(1, 1),
+)
+
+TriggerMuonFilter = cms.EDFilter("PythiaFilterMultiMother", 
+    MaxEta = cms.untracked.double(1.55),
+    MinEta = cms.untracked.double(-1.55),
+    MinPt = cms.untracked.double(6.8), 
+    ParticleID = cms.untracked.int32(13),
+    MotherIDs = cms.untracked.vint32(541, 9900015),
+)
+
+HNLPionFilter = cms.EDFilter("PythiaFilterMultiMother", 
+    MaxEta = cms.untracked.double(10.),
+    MinEta = cms.untracked.double(-10.),
+    MinPt = cms.untracked.double(0.5), 
+    ParticleID = cms.untracked.int32(211),
+    MotherIDs = cms.untracked.vint32(9900015),
+)
+
+HNLDisplacementFilter = cms.EDFilter("MCDisplacementFilter",
+    ParticleIDs = cms.vint32(9900015),
+    LengMin = cms.double(0), 
+    LengMax = cms.double(1300), 
+)
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+    ExternalDecays = cms.PSet(
+        EvtGen130 = cms.untracked.PSet(
+            convertPythiaCodes = cms.untracked.bool(False),
+            decay_table = cms.string('GeneratorInterface/EvtGenInterface/data/DECAY_2014_NOLONGLIFE.DEC'),
+            
+            list_forced_decays = cms.vstring(       
+                'myBc+', 
+                'myBc-',
+                'myHNL_mu',
+                'myHNL_e',
+            ),
+            
+            operates_on_particles = cms.vint32(541, -541, 9900015), 
+            particle_property_file = cms.FileInPath('GeneratorInterface/EvtGenInterface/data/evt_BHNL_mass3.0_ctau100.0_maj.pdl'),
+            user_decay_embedded = cms.vstring(
+              
+               'Alias myB+ B+',
+               'Alias myBc+ B_c+',
+               'Alias myBc- B_c-',
+               'Alias myHNL_mu hnl',
+               'Alias myHNL_e hnl',
+               'ChargeConj myBc+ myBc-',
+               'Decay myBc+',
+               '43.6399154413               mu+    myHNL_mu    PHSP;',
+               '43.6399154413               mu+    myHNL_e     PHSP;',
+               '43.5634313585                e+     myHNL_mu    PHSP;',
+               'Enddecay',
+               'CDecay myBc-',
+               'Decay myHNL_mu',
+               '0.5     mu-    pi+    PHSP;',
+               '0.5     mu+    pi-    PHSP;',
+               'Enddecay',
+               'Decay myHNL_e',
+               '0.5     e-     pi+    PHSP;',
+               '0.5     e+     pi-    PHSP;',
+               'Enddecay',
+               'End',      
+
+            )
+        ),
+        parameterSets = cms.vstring('EvtGen130'),
+    ),
+
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        processParameters = cms.vstring('541:m0 = 6.275',
+                                        '541:tau0 = 0.153',
+                                        ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8CP5Settings',
+                                    'processParameters',
+                                    ),
+    ),
+
+    comEnergy = cms.double(13000.0),
+    maxEventsToPrint = cms.untracked.int32(0),        
+    pythiaHepMCVerbosity = cms.untracked.bool(False), 
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+)
+
+ProductionFilterSequence = cms.Sequence(generator+BFilter+DoubleLeptonFilter+TriggerMuonFilter+HNLPionFilter+HNLDisplacementFilter)
+
